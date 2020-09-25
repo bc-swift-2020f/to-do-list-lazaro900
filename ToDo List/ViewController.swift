347d1b160fc1437d4bbabd9e60d45bf649fdff20
Lazaro Alvelaez <lazaroalvelaez@bc.edu>
2020-09-25 13:51:20 -0400

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Lazaro Alvelaez on 9/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
