1be569672f47ef032957fab77d532cf67874cba9
Lazaro Alvelaez <lazaroalvelaez@bc.edu>
2020-09-27 13:12:34 -0400

    finished 5.6

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Lazaro Alvelaez on 9/27/20.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+    
+}
